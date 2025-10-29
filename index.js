3b7c22d6f5de5812dec07d13f61c3d76f6378858
kemerson091 <114314680+kemerson091@users.noreply.github.com>
2025-10-29 05:55:26 -0300

    criei um html
    criei um html de retorno

--- index.js
+++ index.js
@@ -2,17 +2,62 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Rota 1: Página inicial
 app.get('/', (req, res) => {
-  res.send('Bem-vindo à rota principal!');
+  res.statusCode=200;
+  res.setHeader('Content-Type', 'text/html');
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Página Inicial</title>
+    </head>
+    <body>
+        <h1>Olá, Kemerson! Seu servidor Node.js está funcionando 🎉</h1>
+    </body>
+    </html>
+  `);
 });
 
-// Rota 2: Sobre
-app.get('/sobre', (req, res) => {
-  res.send('Esta é a rota /sobre do servidor Node.js.');
-});
-
-// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// // Rota 1: Página inicial
+// app.get('/', (req, res) => {
+//   res.statusCode=200;
+//   res.writeHead()
+//   res.send(`
+//       <!DOCTYPE html>
+//       <html lang="en">
+//       <head>
+//           <meta charset="UTF-8">
+//           <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//           <title>Document</title>
+//       </head>
+//       <body>
+          
+//       </body>
+//       </html>
+//     `);
+// });
+
+// // Rota 2: Sobre
+// app.get('/sobre', (req, res) => {
+//   res.send('Esta é a rota /sobre do servidor Node.js.');
+// });
+
+// // Inicia o servidor
+// app.listen(PORT, () => {
+//   console.log(`Servidor rodando em http://localhost:${PORT}`);
+// });
